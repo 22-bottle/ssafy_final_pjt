16a7c8b5ca91089bd7c8c344f54cc2146c35dd3b
22-bottle <gmlquddl67@naver.com>
2023-11-17 16:58:52 +0900

    TheMapView

---
+++ theme-map/src/components/map/PlaceDetail.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+
+const props = defineProps({ place: Object });
+
+const place = ref({
+  placeId: '',
+  placeName: '',
+  latitude: '',
+  longitude: '',
+  scoreSum: '',
+  scoreCount: '',
+  address: '',
+  phone: '',
+});
+
+onMounted(() => {
+  place.value = props.place;
+});
+</script>
+
+<template>
+  <div id="detail">
+    <div id="placeInfo">
+      <div>{{ place.placeName }}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#detail {
+  position: fixed;
+  top: 20%;
+  left: 22%;
+  width: 21%;
+  height: 73%;
+  background-color: #f5fffa;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  z-index: 9;
+  border-radius: 5px;
+}
+#placeInfo {
+  position: relative;
+  width: 90%;
+  height: 94%;
+  background-color: #016ef5;
+  border-radius: 5px;
+}
+#placeInfo div {
+  color: white;
+}
+</style>
--- theme-map/src/components/map/PlaceList.vue
+++ theme-map/src/components/map/PlaceList.vue
@@ -1,7 +1,9 @@
 <script setup>
 import { ref, onMounted } from 'vue';
 import { hotPlace } from '@/api/place';
-import PlaceItem from './PlaceItem.vue';
+
+import PlaceItem from '@/components/map/PlaceItem.vue';
+import PlaceDetail from '@/components/map/PlaceDetail.vue';
 
 const hotPlaces = ref([]);
 
@@ -20,6 +22,16 @@ const getHotPlace = () => {
     }
   );
 };
+/* ========> */
+const visibility = ref(false);
+const placeToView = ref(null);
+
+const handleDetail = (place) => {
+  console.log('Enter handleDetail method');
+  visibility.value = !visibility.value;
+  placeToView.value = place;
+};
+/* <======== */
 </script>
 
 <template>
@@ -27,8 +39,21 @@ const getHotPlace = () => {
     <!-- 리스트 -->
     <div class="list">
       <div class="name">내 주변 인기장소</div>
-      <div class="items">
-        <place-item v-for="(place, index) in hotPlaces" :key="place.placeId" :place="place"></place-item>
+      <div class="items scrollbar">
+        <place-item
+          v-for="(place, index) in hotPlaces"
+          :key="place.placeId"
+          :place="place"
+          @detail="handleDetail"
+        ></place-item>
+      </div>
+      <div>
+        <!-- <place-detail v-for="(place, index) in hotPlaces" :key="place.placeId" :place="place"></place-detail> -->
+        <!-- ===========> -->
+        <template v-if="visibility">
+          <place-detail :place="placeToView"></place-detail>
+        </template>
+        <!-- <=========== -->
       </div>
     </div>
   </div>
@@ -38,9 +63,9 @@ const getHotPlace = () => {
 .list {
   position: absolute;
   top: 20%;
-  left: 5%;
+  left: 2%;
   z-index: 10;
-  width: 23%;
+  width: 21%;
   height: 73%;
   background-color: #f5fffa;
   display: flex;
@@ -74,5 +99,10 @@ const getHotPlace = () => {
   display: flex;
   flex-direction: column;
   align-items: center;
+  border-bottom-left-radius: 5px;
+  border-bottom-right-radius: 5px;
+}
+.scrollbar {
+  overflow: auto;
 }
 </style>
