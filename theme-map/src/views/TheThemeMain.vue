12bff30eb39e9ca12700d750d657ab1d6e3f380b
22-bottle <gmlquddl67@naver.com>
2023-11-20 17:37:20 +0900

    tag select

--- theme-map/src/views/TheThemeMain.vue
+++ theme-map/src/views/TheThemeMain.vue
@@ -4,8 +4,8 @@ import { storeToRefs } from 'pinia';
 import { allTags } from '@/api/theme';
 import { useEditorStore } from '@/stores/editor';
 
-import ThemeList from "../components/theme/ThemeList.vue";
-import TagItem from "../components/theme/TagItem.vue";
+import ThemeList from '../components/theme/ThemeList.vue';
+import TagItem from '../components/theme/TagItem.vue';
 
 const tags = ref([]);
 const editorStore = useEditorStore();
@@ -13,58 +13,84 @@ const editorStore = useEditorStore();
 const { isLogin } = storeToRefs(editorStore);
 
 onMounted(() => {
-    getTags();
-})
+  getTags();
+});
 
 const getTags = () => {
-    allTags(
-        ({ data }) => {
-            tags.value = data;
-        },
-        (error) => {
-            console.log(error);
-        }
-    );
+  allTags(
+    ({ data }) => {
+      tags.value = data;
+      tags.value.forEach((tag) => {
+        tag.selected = false;
+      });
+    },
+    (error) => {
+      console.log(error);
+    }
+  );
+};
+
+const selectedTags = ref({ 0: {} });
+const onTagClick = (event) => {
+  const index = event.target.id - 1;
+  const tag = tags.value[index];
+  tag.selected = !tag.selected;
+  if (tag.selected) {
+    selectedTags.value[tag.tagId] = tag;
+  } else {
+    delete selectedTags.value[tag.tagId];
+  }
+  console.log(Object.values(selectedTags.value));
 };
 </script>
 
 <template>
-    <div id="container">
-        <div id="header">
-            <div class="title">테마로 보는 대전</div>
-            <template v-if="isLogin">
-                <button>
-                    <router-link :to="{ name: 'create' }">새 테마 등록하기</router-link>
-                </button>
-            </template>
-        </div>
-        <div id="hotThemes">
-            <div>인기 테마 Top 5</div>
-            <theme-list type="hot"></theme-list>
-        </div>
-        <div id="allThemes">
-            <div>전체 테마</div>
-            <div id="tags">
-                <tag-item v-for="(tag, index) in tags" :key="tag.tagId" :tag="tag"></tag-item>
-            </div>
-            <theme-list type="all"></theme-list>
-        </div>
+  <div id="container">
+    <div id="header">
+      <div class="title">테마로 보는 대전</div>
+      <template v-if="isLogin">
+        <button>
+          <router-link :to="{ name: 'create' }">새 테마 등록하기</router-link>
+        </button>
+      </template>
     </div>
+    <div id="hotThemes">
+      <div>인기 테마 Top 5</div>
+      <theme-list type="hot"></theme-list>
+    </div>
+    <div id="allThemes">
+      <div>전체 테마</div>
+      <div id="tags">
+        <tag-item
+          v-for="(tag, index) in tags"
+          :key="tag.tagId"
+          :tag="tag"
+          :class="{ selected: tag.selected }"
+          @click="onTagClick"
+          :id="tag.tagId"
+        ></tag-item>
+      </div>
+      <theme-list type="all" :tags="selectedTags.value"></theme-list>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 #container {
-    margin-top: 3%;
-    padding: 8%;
+  margin-top: 3%;
+  padding: 8%;
 }
 #header {
-    font-size: 40px;
-    display: flex;
-    flex-direction: row;
-    justify-content: space-between;
+  font-size: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
 }
 #tags {
-    display: flex;
-    flex-direction: row;
+  display: flex;
+  flex-direction: row;
+}
+.selected {
+  color: red;
 }
 </style>
